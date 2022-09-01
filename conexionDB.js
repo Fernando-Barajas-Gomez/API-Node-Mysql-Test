e689390903067a5f4d8380a9d42a0b4caf6ca881
Fernando-Barajas-Gomez <fernando.bgomez@alumnos.udg.mx>
2022-09-01 13:44:47 -0500

    Archivo para generar la conexion a la base de datos

---
+++ conexionDB.js
@@ -0,0 +1,16 @@
+//----- Importamos los modulos -----//
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
+
+//----- Parametros para crear la conexion (Se obtienen de las variables de entorno)-----//
+const conexion = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    connectionLimit: 5
+});
+
+//----- Exportamos el modulo -----//
+module.exports = conexion;
