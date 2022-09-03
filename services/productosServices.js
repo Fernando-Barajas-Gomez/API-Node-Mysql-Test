80d6b2d1b1dbfc7558b4ad151900c0f7adca8a34
Fernando-Barajas-Gomez <fernando.bgomez@alumnos.udg.mx>
2022-09-02 21:58:52 -0500

    se agrego una funcion para obtener cierto producto

--- services/productosServices.js
+++ services/productosServices.js
@@ -1,8 +1,8 @@
 //----- Importamos el archivo de conexion a la DB -----//
 const conexion = require('../conexionDB');
 
-//----- Exportamos los datos al controlador -----//
-exports.obtenerProductos = () => {
+/**Función para obtener todos los productos de la base de datos */
+const obtenerProductos = () => {
     //retornamos los datos a traves de una promesa
     return new Promise ((resolve, reject) => {
         //generamos la consulta
@@ -15,3 +15,21 @@ exports.obtenerProductos = () => {
         });
     }); 
 }
+
+/**Función para obtener cierto producto de la base de datos */
+const obtenerProducto = (producto) => {
+    //retornamos los datos a traves de una promesa
+    return new Promise ((resolve, reject) => {
+        //generamos la consulta
+        conexion.query('SELECT product.*, category.name AS categoryName FROM product, category WHERE product.category=category.id AND product.name LIKE?', ['%'+producto+'%'], (error, filas, campos) => {
+            if(!error){ //si la consulta no tuvo errores
+                resolve(filas); //retornamos los productos
+            }else{
+                reject('Error al obtener los productos');
+            }
+        });
+    }); 
+}
+//----- Exportamos las funciones al controlador -----//
+exports.obtenerProductos = obtenerProductos;
+exports.obtenerProducto = obtenerProducto;
