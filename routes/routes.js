da0003b45be5e59e97ec46cc63b3ee33354d229d
Fernando-Barajas-Gomez <fernando.bgomez@alumnos.udg.mx>
2022-09-04 00:38:52 -0500

    se agrego ruta para obtener cierto producto de cierta categoria

--- routes/routes.js
+++ routes/routes.js
@@ -5,13 +5,14 @@ const router = Router();
 //----- Importamos los controladores -----//
 const productos = require('../controllers/productosController');
 const categorias = require('../controllers/categoriasController');
+const categoProduc = require('../controllers/categoProducController')
 
 //----- Ruta inicial -----//
 router.get('/', (req, res)  => {
     return res.status(200).json({message: "OK. Bienvenido a la api." });
 });
 
-//----- Ruta para obtener los productos -----//
+//----- Ruta para obtener todos los productos -----//
 router.get('/products', productos.obtenerProductos);
 
 //----- Ruta para obtener cierto producto -----//
@@ -22,6 +23,10 @@ router.get('/categories', categorias.obtenerCategorias);
 
 //----- Ruta para obtener los productos de cierta categoria -----//
 router.get('/category/:categoria', categorias.obtenerCategoria);
+
+//----- Ruta para obtener cierto producto de cierta categoria -----//
+router.get('/product/category/:producto/:categoria', categoProduc.obtenerProducto);
+
 
 //----- Exportamos el modulo -----//
 module.exports = router;
